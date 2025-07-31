c18af723680e2c8c620bdf269143e0458dcfa8bc
MrAnalyticals <dontletmondaygetyoudown@gmail.com>
2025-07-31 22:54:30 +0100

    Update startup.sh to use PORT environment variable

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export PYTHONUNBUFFERED=1
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+# Use the PORT environment variable if available, otherwise default to 8000
+PORT=${PORT:-8000}
+uvicorn app.main:app --host 0.0.0.0 --port $PORT
